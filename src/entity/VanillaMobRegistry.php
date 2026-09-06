7e0c40b125cdd90f842e47a33b0883bd37258e6c
GravityGDev <fuzionbukkit+1@gmail.com>
2026-09-06 14:33:16 +0100

    Register Stray Bogged and Parched entities

--- src/entity/VanillaMobRegistry.php
+++ src/entity/VanillaMobRegistry.php
@@ -34,6 +34,10 @@ final class VanillaMobRegistry{
 	}
 
 	public static function register(EntityFactory $factory) : void{
+		$factory->register(Bogged::class, function(World $world, CompoundTag $nbt) : Bogged{
+			return new Bogged(EntityDataHelper::parseLocation($nbt, $world), $nbt);
+		}, ['Bogged', 'minecraft:bogged']);
+
 		$factory->register(CaveSpider::class, function(World $world, CompoundTag $nbt) : CaveSpider{
 			return new CaveSpider(EntityDataHelper::parseLocation($nbt, $world), $nbt);
 		}, ['CaveSpider', 'minecraft:cave_spider']);
@@ -46,9 +50,17 @@ final class VanillaMobRegistry{
 			return new Husk(EntityDataHelper::parseLocation($nbt, $world), $nbt);
 		}, ['Husk', 'minecraft:husk']);
 
+		$factory->register(Parched::class, function(World $world, CompoundTag $nbt) : Parched{
+			return new Parched(EntityDataHelper::parseLocation($nbt, $world), $nbt);
+		}, ['Parched', 'minecraft:parched']);
+
 		$factory->register(Silverfish::class, function(World $world, CompoundTag $nbt) : Silverfish{
 			return new Silverfish(EntityDataHelper::parseLocation($nbt, $world), $nbt);
 		}, ['Silverfish', 'minecraft:silverfish']);
+
+		$factory->register(Stray::class, function(World $world, CompoundTag $nbt) : Stray{
+			return new Stray(EntityDataHelper::parseLocation($nbt, $world), $nbt);
+		}, ['Stray', 'minecraft:stray']);
 
 		$factory->register(WitherSkeleton::class, function(World $world, CompoundTag $nbt) : WitherSkeleton{
 			return new WitherSkeleton(EntityDataHelper::parseLocation($nbt, $world), $nbt);
