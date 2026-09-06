65b55068bd415adc3406446af9c13d7bd63b8fb6
GravityGDev <fuzionbukkit+1@gmail.com>
2026-09-06 13:19:01 +0100

    Add wither skeleton melee and wither effect behaviour

---
+++ src/entity/WitherSkeleton.php
@@ -0,0 +1,77 @@
+<?php
+
+/*
+ *
+ *  ____            _        _   __  __ _                  __  __ ____
+ * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
+ * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
+ * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
+ * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
+
+declare(strict_types=1);
+
+namespace pocketmine\entity;
+
+use pocketmine\entity\ai\goal\LookAtPlayerGoal;
+use pocketmine\entity\ai\goal\MeleeAttackGoal;
+use pocketmine\entity\ai\goal\NearestPlayerTargetGoal;
+use pocketmine\entity\ai\goal\RandomStrollGoal;
+use pocketmine\entity\effect\EffectInstance;
+use pocketmine\entity\effect\VanillaEffects;
+use pocketmine\item\Item;
+use pocketmine\item\VanillaItems;
+use pocketmine\network\mcpe\protocol\types\entity\EntityIds;
+use pocketmine\player\Player;
+use function mt_rand;
+
+class WitherSkeleton extends Skeleton{
+	public static function getNetworkTypeId() : string{ return EntityIds::WITHER_SKELETON; }
+
+	protected function getInitialSizeInfo() : EntitySizeInfo{
+		return new EntitySizeInfo(2.01, 0.72);
+	}
+
+	protected function burnsInDaylight() : bool{
+		return false;
+	}
+
+	public function isFireProof() : bool{
+		return true;
+	}
+
+	protected function registerGoals() : void{
+		$this->getTargetSelector()->addGoal(1, new NearestPlayerTargetGoal($this, 35.0));
+		$this->getGoalSelector()->addGoal(2, new MeleeAttackGoal(
+			$this,
+			0.125,
+			4.0,
+			2.0,
+			function(Mob $_mob, Player $target) : void{
+				$target->getEffects()->add(new EffectInstance(VanillaEffects::WITHER(), 10 * 20));
+			}
+		));
+		$this->getGoalSelector()->addGoal(7, new RandomStrollGoal($this, 0.1, 8, 80));
+		$this->getGoalSelector()->addGoal(8, new LookAtPlayerGoal($this, 8.0));
+	}
+
+	public function getName() : string{
+		return "Wither Skeleton";
+	}
+
+	public function getDrops() : array{
+		return [
+			VanillaItems::BONE()->setCount(mt_rand(0, 2)),
+			VanillaItems::COAL()->setCount(mt_rand(0, 1))
+		];
+	}
+
+	public function getPickedItem() : ?Item{
+		return VanillaItems::WITHER_SKELETON_SPAWN_EGG();
+	}
+}
