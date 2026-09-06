997dbb6f958030be29661e07d03fb72954ee1b76
GravityGDev <fuzionbukkit+1@gmail.com>
2026-09-06 12:20:20 +0100

    Add vanilla-style skeleton entity

---
+++ src/entity/Skeleton.php
@@ -0,0 +1,95 @@
+<?php
+
+/*
+ *
+ *  ____            _        _   __  __ _                  __  __ ____
+ * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
+ * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
+ * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
+ * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
+
+declare(strict_types=1);
+
+namespace pocketmine\entity;
+
+use pocketmine\entity\ai\goal\LookAtPlayerGoal;
+use pocketmine\entity\ai\goal\NearestPlayerTargetGoal;
+use pocketmine\entity\ai\goal\RandomStrollGoal;
+use pocketmine\entity\ai\goal\RangedAttackGoal;
+use pocketmine\item\Item;
+use pocketmine\item\VanillaItems;
+use pocketmine\nbt\tag\CompoundTag;
+use pocketmine\network\mcpe\protocol\types\entity\EntityIds;
+use pocketmine\world\World;
+use function mt_rand;
+
+class Skeleton extends Mob{
+	private const TAG_NATURALLY_SPAWNED = "NaturallySpawned"; //TAG_Byte
+
+	private bool $naturallySpawned = false;
+
+	public static function getNetworkTypeId() : string{ return EntityIds::SKELETON; }
+
+	protected function getInitialSizeInfo() : EntitySizeInfo{
+		return new EntitySizeInfo(1.9, 0.6);
+	}
+
+	protected function initEntity(CompoundTag $nbt) : void{
+		parent::initEntity($nbt);
+		$this->naturallySpawned = $nbt->getByte(self::TAG_NATURALLY_SPAWNED, 0) !== 0;
+	}
+
+	public function saveNBT() : CompoundTag{
+		$nbt = parent::saveNBT();
+		$nbt->setByte(self::TAG_NATURALLY_SPAWNED, $this->naturallySpawned ? 1 : 0);
+		return $nbt;
+	}
+
+	public function isNaturallySpawned() : bool{
+		return $this->naturallySpawned;
+	}
+
+	public function setNaturallySpawned(bool $naturallySpawned = true) : void{
+		$this->naturallySpawned = $naturallySpawned;
+	}
+
+	protected function registerGoals() : void{
+		$this->getTargetSelector()->addGoal(1, new NearestPlayerTargetGoal($this, 32.0));
+		$this->getGoalSelector()->addGoal(2, new RangedAttackGoal($this, 0.1, 15.0));
+		$this->getGoalSelector()->addGoal(7, new RandomStrollGoal($this, 0.08, 8, 80));
+		$this->getGoalSelector()->addGoal(8, new LookAtPlayerGoal($this, 8.0));
+	}
+
+	protected function entityBaseTick(int $tickDiff = 1) : bool{
+		if($this->getWorld()->getDifficulty() === World::DIFFICULTY_PEACEFUL){
+			$this->flagForDespawn();
+		}
+
+		return parent::entityBaseTick($tickDiff);
+	}
+
+	public function getName() : string{
+		return "Skeleton";
+	}
+
+	public function getDrops() : array{
+		return [
+			VanillaItems::ARROW()->setCount(mt_rand(0, 2)),
+			VanillaItems::BONE()->setCount(mt_rand(0, 2))
+		];
+	}
+
+	public function getXpDropAmount() : int{
+		return 5;
+	}
+
+	public function getPickedItem() : ?Item{
+		return VanillaItems::SKELETON_SPAWN_EGG();
+	}
+}
