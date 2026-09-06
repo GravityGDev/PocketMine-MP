ca3d071b05c39b8a18bd24df12ac7d6fc2b8c978
GravityGDev <fuzionbukkit+1@gmail.com>
2026-09-06 14:32:59 +0100

    Add Parched weakness-arrow combat entity

---
+++ src/entity/Parched.php
@@ -0,0 +1,65 @@
+<?php
+
+/*
+ *
+ *  ____            _        _   __  __ _                  __  __ ____
+ * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
+ * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
+ * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
+ * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
+
+declare(strict_types=1);
+
+namespace pocketmine\entity;
+
+use pocketmine\entity\ai\goal\LookAtPlayerGoal;
+use pocketmine\entity\ai\goal\NearestPlayerTargetGoal;
+use pocketmine\entity\ai\goal\RandomStrollGoal;
+use pocketmine\entity\ai\goal\RangedAttackGoal;
+use pocketmine\entity\effect\Effect;
+use pocketmine\entity\effect\EffectImmunity;
+use pocketmine\entity\effect\EffectInstance;
+use pocketmine\entity\effect\VanillaEffects;
+use pocketmine\nbt\tag\CompoundTag;
+use pocketmine\network\mcpe\protocol\types\entity\EntityIds;
+
+class Parched extends Skeleton implements EffectImmunity{
+	public static function getNetworkTypeId() : string{ return EntityIds::PARCHED; }
+
+	protected function initEntity(CompoundTag $nbt) : void{
+		$this->setMaxHealth(16);
+		parent::initEntity($nbt);
+	}
+
+	protected function burnsInDaylight() : bool{
+		return false;
+	}
+
+	public function isImmuneToEffect(Effect $effect) : bool{
+		return $effect === VanillaEffects::WEAKNESS();
+	}
+
+	protected function registerGoals() : void{
+		$this->getTargetSelector()->addGoal(1, new NearestPlayerTargetGoal($this, 32.0));
+		$this->getGoalSelector()->addGoal(2, new RangedAttackGoal(
+			$this,
+			0.1,
+			15.0,
+			70,
+			50,
+			new EffectInstance(VanillaEffects::WEAKNESS(), 30 * 20)
+		));
+		$this->getGoalSelector()->addGoal(7, new RandomStrollGoal($this, 0.08, 8, 80));
+		$this->getGoalSelector()->addGoal(8, new LookAtPlayerGoal($this, 8.0));
+	}
+
+	public function getName() : string{
+		return "Parched";
+	}
+}
