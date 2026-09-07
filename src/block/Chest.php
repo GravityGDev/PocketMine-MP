6ce7ee5cd9c7694a092a0f0130bf92518c5b8045
GravityGDev <fuzionbukkit+1@gmail.com>
2026-09-07 22:52:19 +0100

    Add chest waterlogging parity

--- src/block/Chest.php
+++ src/block/Chest.php
@@ -34,8 +34,13 @@ use pocketmine\math\Facing;
 use pocketmine\math\Vector3;
 use pocketmine\player\Player;
 
-class Chest extends Transparent implements HorizontalFacing{
+class Chest extends Transparent implements HorizontalFacing, Waterloggable{
 	use FacesOppositePlacingPlayerTrait;
+	use WaterloggableTrait;
+
+	public function canBeWaterlogged() : bool{
+		return true;
+	}
 
 	protected function recalculateCollisionBoxes() : array{
 		//these are slightly bigger than in PC
